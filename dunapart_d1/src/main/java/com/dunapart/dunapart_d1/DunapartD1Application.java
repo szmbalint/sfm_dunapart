60216d805f1189a08fff6d214d01f2ff2a869b92
Mark Toldi <102686020+MarkToldi@users.noreply.github.com>
2024-11-02 14:37:27 +0100

    Add files via upload

---
+++ dunapart_d1/src/main/java/com/dunapart/dunapart_d1/DunapartD1Application.java
@@ -0,0 +1,13 @@
+package com.dunapart.dunapart_d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunapartD1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunapartD1Application.class, args);
+	}
+
+}
---
+++ dunapart_d1/src/test/java/com/dunapart/dunapart_d1/DunapartD1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunapart.dunapart_d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunapartD1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
