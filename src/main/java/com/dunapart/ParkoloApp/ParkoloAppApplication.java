9b08f54b175c7066ac546ed7370686907825318e
szmbalint <balintszentmiklossy@gmail.com>
2024-11-02 15:31:16 +0100

    asd

---
+++ src/main/java/com/dunapart/ParkoloApp/ParkoloAppApplication.java
@@ -0,0 +1,13 @@
+package com.dunapart.ParkoloApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkoloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkoloAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dunapart/ParkoloApp/ParkoloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunapart.ParkoloApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkoloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
